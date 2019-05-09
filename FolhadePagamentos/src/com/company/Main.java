8ac055b41550287087096403f76d20b1dc259d72
WilliamPhilippe <wplb@ic.ufal.br>
2019-05-08 22:06:26 -0300

    up

--- FolhadePagamentos/src/com/company/Main.java
+++ FolhadePagamentos/src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Menu myMenu = new Menu();
+
+        while(myMenu.runMenu());
     }
 }
---
+++ FolhadePagamentos/src/com/company/Menu.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+    public boolean runMenu(){
+        System.out.println("O que deseja fazer?");
+        System.out.println("1 - Adicionar um empregado.");
+        System.out.println("2 - Remover um empregado.");
+        System.out.println("3 - Lancar um cartao de ponto.");
+        System.out.println("4 - Lancar resultado de vendas.");
+        System.out.println("5 - Lancar uma taxa de servico.");
+        System.out.println("6 - Alterar detalhes de um empregado.");
+        System.out.println("7 - Rodar folha de pagamento.");
+        System.out.println("8 - Agenda de pagamento.");
+        System.out.println("9 - Criar nova agenda de pagamentos.");
+        System.out.println("10 - Refazer.");
+        System.out.println("11 - Desfazer.");
+        System.out.println("0 - Encerrar sistema.");
+
+        Scanner input = new Scanner(System.in);
+        Integer option = input.nextInt();
+
+
+
+        return (option != 0 ? true : false);
+    }
+}

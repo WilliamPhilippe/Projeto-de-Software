a3d7878042a0b6230608bd98eaa10d2efd0fa79b
WilliamPhilippe <wplb@ic.ufal.br>
2019-05-09 20:17:39 -0300

    up

--- FolhadePagamentos/src/com/company/Menu.java
+++ FolhadePagamentos/src/com/company/Menu.java
@@ -21,8 +21,47 @@ public class Menu {
         Scanner input = new Scanner(System.in);
         Integer option = input.nextInt();
 
+        switch (option){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+        }
 
 
+        System.out.println();
+        System.out.println();
         return (option != 0 ? true : false);
     }
+
+
 }
